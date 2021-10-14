a2001149b71b65badbc7228dfd05f6951ac3ee4c
Diego Bruno <diegobc.21@hotmail.com>
2021-10-14 11:59:23 -0300

    Deleted finished from course model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,4 +6,3 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
-    finished = models.BooleanField()
